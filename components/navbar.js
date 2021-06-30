c71761ad9d31ddd06c4c09b77b8cb571579b2c72
Grail Willy <wlygrl@gmail.com>
2021-06-30 20:23:45 +0800

    Add navbar component

---
+++ components/navbar.js
@@ -0,0 +1,26 @@
+class Navbar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <nav class="navbar navbar-expand-md navbar-light fixed-top">
+        <a href="/" class="navbar-brand">Joi Willi</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <div class="navbar-nav ml-auto">
+            <a href="./pages/blog.html" class="nav-item nav-link">Blog</a>
+            <a href="#" class="nav-item nav-link">Portfolio</a>
+            <a href="#" class="nav-item nav-link">Service</a>
+            <a href="#" class="nav-item nav-link">Contact</a>
+          </div>
+        </div>
+      </nav>
+    `;
+  }
+}
+
+customElements.define('navbar-component', Navbar);
