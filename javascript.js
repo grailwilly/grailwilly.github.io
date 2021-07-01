e5943c510955754d239eeeff179d5c73f52adabd
Grail Willy <wlygrl@gmail.com>
2021-07-01 21:43:07 +0800

    Add component and links

--- javascript.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.nav-link').forEach(link => {
-    if (link.getAttribute('href').toLowerCase() === location.pathname.toLowerCase()) {
-      link.classList.add('active-link');
-    } else {
-      link.classList.remove('active-link');
-    }
-  })
-})
