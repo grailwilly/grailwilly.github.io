0c616dd8c0ab792e9f55e187f2a4a884a7383460
Grail Willy <wlygrl@gmail.com>
2021-07-02 22:44:25 +0800

    Uncomment link

--- components/navbar.js
+++ components/navbar.js
@@ -12,8 +12,8 @@ class Navbar extends HTMLElement {
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <div class="navbar-nav ml-auto">
-            <!--<a href="./blog.html" class="nav-item nav-link">Blog</a>
-            <a href="./portfolio.html" class="nav-item nav-link">Portfolio</a>
+            <a href="./pages/blog.html" class="nav-item nav-link">Blog</a>
+            <!--<a href="./portfolio.html" class="nav-item nav-link">Portfolio</a>
             <a href="./contact.html" class="nav-item nav-link">Contact</a>-->
           </div>
         </div>
